78d0fcbf1b340b8b6fe74c135ad91a62ee58d41c
Richti <richard.gruet75@gmail.com>
2017-02-17 13:11:52 +0100

    bug fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
 function goToUser(id){
-	document.location.href="http://localhost:3001/users/" + id;
+	document.location.href="http://ec2-52-54-102-237.compute-1.amazonaws.com:3001/users/" + id;
 }
 
 function goToAlliance(id){
-	document.location.href="http://localhost:3001/alliances/" + id;
+	document.location.href="http://ec2-52-54-102-237.compute-1.amazonaws.com:3001/alliances/" + id;
 }
 
 function goToCharacter(id){
-	document.location.href="http://localhost:3001/characters/" + id;
+	document.location.href="http://ec2-52-54-102-237.compute-1.amazonaws.com:3001/characters/" + id;
 }
